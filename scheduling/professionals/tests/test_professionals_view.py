a31110b709b7dfff488c7646fd1985afb4ad496e
Váltson Araújo <valtson88@gmail.com>
2025-02-12 22:03:25 -0300

    Projeto atualizado

--- scheduling/professionals/tests/test_professionals_view.py
+++ scheduling/professionals/tests/test_professionals_view.py
@@ -7,9 +7,9 @@ from django.contrib.auth.models import User
 def test_professional_create():
     # Simula um cliente HTTP
     client = APIClient()
-    # Obtém a url
+    
     url = reverse("professional_create")
-    # Faz a requisição post
+    
     response = client.post(url,
         {
             "name": "joão",
@@ -26,16 +26,16 @@ def test_professional_create():
 
 @pytest.mark.django_db
 def test_professional_list():
-    # Simula um cliente HTTP
+    
     client = APIClient()
     # Cria um usuário de teste
     user = User.objects.create_user(username="testuser", password="testpass")
-    # Obtém a url
+    
     url = reverse("professionals_list")
-    # Faz o login
+    
     client.login(username="testuser", password="testpass")
-    # Faz a requisição get
+    
     response = client.get(url)
-    # Verifica se o status retornado foi 200 OK
+    
     assert response.status_code == 200
     
