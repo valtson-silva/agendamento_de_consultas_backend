c02aeda9712fb8d8dfa6644ad13ff181c4373e57
Váltson Araújo <valtson88@gmail.com>
2025-02-12 21:54:27 -0300

    Projeto atualizado

--- scheduling/professionals/view_specialty_professional.py
+++ scheduling/professionals/view_specialty_professional.py
@@ -12,24 +12,22 @@ class ProfessionalSpecialtyCreateView(APIView):
     permission_classes = [IsAuthenticated]
         
     def post(self, request):
-        # Serializa os dados recebidos no corpo da requisição
+        
         serializer = ProfessionalSpecialtySerializer(data=request.data)
-        # Verifica se os dados são válidos
+    
         if serializer.is_valid():
-            # Salva as informções no banco de dados
+            
             serializer.save()
             
-            # Retorna as informções e o status 201
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             
-            # Retorna error e o status 400
             return Response({"error": "Os dados informados são inválidos."}, status=status.HTTP_400_BAD_REQUEST)
         
 class ProfessionalSpecialtyListView(APIView):
     # Mostra todas as especialidades de um profissional
     
-    # verifica se o usuário está logado
+    
     permission_classes = [IsAuthenticated]
     
     def get(self, request, professional_id):
@@ -52,14 +50,13 @@ class ProfessionalSpecialtyListView(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         except:
             
-            # Retorna error e o status 404
             return Response({"error": "Não existe um profissional com esse ID."}, status=status.HTTP_404_NOT_FOUND)
             
             
 class SpecialtyProfessionalListView(APIView):
     # Mostra todos os profissionais de uma especialidade
     
-    # verifica se o usuário está logado
+
     permission_classes = [IsAuthenticated]
     
     def get(self, request, specialty_id):
@@ -82,53 +79,50 @@ class SpecialtyProfessionalListView(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         except:
             
-            # Retorna error e o status 404
             return Response({"error": "Não existe uma especialidade com esse ID."}, status=status.HTTP_404_NOT_FOUND)
             
 class ProfessionalSpecialtyUpdateView(APIView):
     # Atualiza as informações do registro
     
-    # verifica se o usuário está logado
+    
     permission_classes = [IsAuthenticated]
     
     def put(self, request, id):
         try:
-            # Tenta obter o registro
+            
             professionalSpecialty = ProfessionalSpecialty.objects.get(id=id)
-            # Serializa as alterações
+            
             serializer = ProfessionalSpecialtySerializer(professionalSpecialty, data=request.data)
             
-            # Verifica se as alterações são válidas
+            
             if serializer.is_valid():
-                # Salva as alterações no banco de dados
+                
                 serializer.save()
                 
-                # Retorna o registro atualizado e o status 200
                 return Response(serializer.data, status=status.HTTP_200_OK)
             else:
-                # Retorna error e o status 400
+                
                 return Response({"error": "Dados informados inválidos"}, status=status.HTTP_400_BAD_REQUEST)
         except:
-            # Retorna error e o status 404
+            
             return Response({"error": "Não existe um registro com esse ID."}, status=status.HTTP_404_NOT_FOUND)
       
       
 class ProfessionalSpecialtyDeleteView(APIView):
     # Deleta um registro do banco de dados
     
-    # verifica se o usuário está logado
+    
     permission_classes = [IsAuthenticated]
     
     def delete(self, request, id):
         try:
-            # Tenta obter o registro
+            
             professionalSpecialty = professionalSpecialty.objects.get(id=id)
-            # Deleta o registro
+            
             professionalSpecialty.delete()
             
-            # Retorna sucesso e o status 200
             return Response({"success": "Registro deletado com sucesso."}, status=status.HTTP_200_OK)
         except:
-            # Retorna error e o status 404
+            
             return Response({"error": "Não existe um registro com esse ID."}, status=status.HTTP_404_NOT_FOUND)  
         
